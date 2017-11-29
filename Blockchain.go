9de39f8d821f92f21e1c5ada820d992c4097fb55
Jose Alfredo Alvarez Aldana <josealfredo1515@gmail.com>
2017-11-29 18:15:31 +0100

    Major changes for v0.2.

--- Blockchain.go
+++ Blockchain.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"github.com/op/go-logging"
-	"net"
 	"github.com/chepeftw/bchainlibs"
-	"os"
 	"github.com/chepeftw/treesiplibs"
+	"net"
+	"os"
 	"encoding/json"
 	"time"
 	"strconv"
@@ -18,23 +18,20 @@ import (
 var log = logging.MustGetLogger("blockchain")
 
 // +++++++++ Global vars
-var me net.IP = net.ParseIP(bchainlibs.LocalhostAddr)
-var cryptoPiece = "00"
+var me = net.ParseIP(bchainlibs.LocalhostAddr)
 var rootNode = "10.12.0.1"
 var randomGen = rand.New( rand.NewSource( time.Now().UnixNano() ) )
 
 // +++++++++ Channels
-// For the Miner the Input and Output will be to Router
 var input = make(chan string)
 var output = make(chan string)
 var done = make(chan bool)
 
 var blockchain []bchainlibs.Packet
-var blockchain_length int
-var queries map[string]bchainlibs.Packet = make(map[string]bchainlibs.Packet)
+var queries = make(map[string]bchainlibs.Packet)
 
 func toOutput(payload bchainlibs.Packet) {
-	log.Debug("Sending Packet with TID " + payload.TID + " to channel output")
+	log.Debug("Sending Packet with ID " + payload.ID + " to channel output")
 	bchainlibs.SendGeneric( output, payload, log )
 }
 
@@ -60,25 +57,29 @@ func resolveQuery() {
 	// 	Collect data
 	// 	SendData -> val := true
 
-	packet := bchainlibs.AssembleUnverifiedBlock(me, "function")
+	packet := bchainlibs.BuildTransaction(me, "function")
 	sendIt := false
 
 	switch me.String() {
 	case node1 :
-		packet.Block.ActualHop = net.ParseIP( node1 )
-		packet.Block.PreviousHop = net.ParseIP( bchainlibs.NullhostAddr )
+		packet.Transaction.Data.Order = 1
+		packet.Transaction.Data.ActualHop = net.ParseIP( node1 )
+		packet.Transaction.Data.PreviousHop = net.ParseIP( bchainlibs.NullhostAddr )
 		sendIt = true
 	case node2 :
-		packet.Block.ActualHop = net.ParseIP( node2 )
-		packet.Block.PreviousHop = net.ParseIP( node1 )
+		packet.Transaction.Data.Order = 2
+		packet.Transaction.Data.ActualHop = net.ParseIP( node2 )
+		packet.Transaction.Data.PreviousHop = net.ParseIP( node1 )
 		sendIt = true
 	case node3 :
-		packet.Block.ActualHop = net.ParseIP( node3 )
-		packet.Block.PreviousHop = net.ParseIP( node2 )
+		packet.Transaction.Data.Order = 3
+		packet.Transaction.Data.ActualHop = net.ParseIP( node3 )
+		packet.Transaction.Data.PreviousHop = net.ParseIP( node2 )
 		sendIt = true
 	case node4 :
-		packet.Block.ActualHop = net.ParseIP( node4 )
-		packet.Block.PreviousHop = net.ParseIP( node3 )
+		packet.Transaction.Data.Order = 4
+		packet.Transaction.Data.ActualHop = net.ParseIP( node4 )
+		packet.Transaction.Data.PreviousHop = net.ParseIP( node3 )
 		sendIt = true
 	}
 
@@ -101,82 +102,75 @@ func attendInputChannel() {
 			json.Unmarshal([]byte(j), &payload)
 
 			//source := payload.Source
-			tid := payload.TID
+			id := payload.ID
 
-			//log.Debug("Incoming payload with TID = " + tid)
+			//log.Debug("Incoming payload with ID = " + id)
 
 			switch payload.Type {
 
 			case bchainlibs.QueryType:
 				log.Debug("Packet with QueryType")
-				queries[tid] = payload
+				queries[id] = payload
 				resolveQuery()
 			break
 
-			case bchainlibs.VBlockType:
-				log.Debug("Packet with VBlockType, with PrID " + payload.PrID + " TID " + payload.TID + " Salt " + payload.Salt)
-				log.Debug("CryptoPiece is " + cryptoPiece)
+			case bchainlibs.BlockType:
+				log.Debug("Packet with BlockType, with PacketID: " + payload.ID + ", BlockID: " + payload.Block.ID )
+				log.Debug("Previous Block ID: " + payload.Block.PreviousID + ", Nonce: " + payload.Block.Nonce + ", MerkleTreeRoot " + payload.Block.MerkleTreeRoot)
 
-				if payload.IsValid(cryptoPiece) {
+				// Checking the actual last block of the blockchain against the received one
+				// The bigger block should be the new one
+				payloadIsValid := false
 
-					// Checking the actual last block of the blockchain against the received one
-					// The bigger block should be the new one
-					payloadIsValid := false
+				if len( blockchain) > 0 {
+					lastB := blockchain[len(blockchain)-1]
 
-					if len( blockchain) > 0 {
-						lastB := blockchain[blockchain_length-1]
-
-						// The check for the created time needs some improvements.
-						//if payload.PrID == lastB.BID && payload.Block.Created > lastB.Block.Created {
-						if payload.PrID == lastB.BID {
-							payloadIsValid = true
-						}
-					} else if len( blockchain ) == 0 {
+					// The check for the created time needs some improvements.
+					//if payload.PrID == lastB.BID && payload.Block.Created > lastB.Block.Created {
+					if payload.Block.PreviousID == lastB.Block.ID {
 						payloadIsValid = true
-					} else {
-						log.Debug("Stranger things are happening!")
 					}
+				} else if len( blockchain ) == 0 {
+					payloadIsValid = true
+				} else {
+					log.Debug("Stranger things are happening!")
+				}
 
-					if payloadIsValid {
+				if payloadIsValid {
 
-						log.Debug("Payload IS Valid")
-						blockchain = append( blockchain, payload )
-						blockchain_length = len( blockchain )
+					log.Debug("Payload IS Valid")
+					blockchain = append( blockchain, payload )
 
-						copyPayload := payload.Duplicate()
-						copyPayload.Type = bchainlibs.LastBlockType
-						toOutput(copyPayload) // SendLastBlock() basically
+					copyPayload := payload
+					copyPayload.Type = bchainlibs.LastBlockType
+					toOutput(copyPayload) // SendLastBlock() basically
 
-						log.Debug("----- This is the blockchain")
-						for index, element := range blockchain {
-							log.Debug( string(index) + " "+ element.String() )
-						}
-						log.Debug("----- --------")
-
-						// IF I'm the query generator, does this solves my query?
-						// checkQueryCompleteness()?
-						if payload.Block.ActualHop.String() == payload.Block.Destination.String() {
-							// After a validated block, just re run everything to get new data
-							log.Debug("QUERY COMPLETED, the blockchain length is " + strconv.Itoa(blockchain_length) )
-							log.Info("QUERY_END=" + strconv.FormatInt(time.Now().UnixNano(), 10))
-							log.Debug("PLEASE_EXIT=1234")
-						}
-
-					} else {
-						log.Debug("Payload NOT Valid")
+					log.Debug("----- This is the blockchain")
+					for index, element := range blockchain {
+						log.Debug( string(index) + " "+ element.String() )
 					}
+					log.Debug("----- --------")
+
+					// This is basically to end the program BUT I need to find another way
+
+					// IF I'm the query generator, does this solves my query?
+					// checkQueryCompleteness()?
+					//if payload.Block.ActualHop.String() == payload.Block.Destination.String() {
+					//	// After a validated block, just re run everything to get new data
+					//	log.Debug("QUERY COMPLETED, the blockchain length is " + strconv.Itoa(len(blockchain)) )
+					//	log.Info("QUERY_END=" + strconv.FormatInt(time.Now().UnixNano(), 10))
+					//	log.Debug("PLEASE_EXIT=1234")
+					//}
 
 				} else {
 					log.Debug("Payload NOT Valid")
-
-					// Then we re-add it to the unverified stuff maybe?
-
 				}
+
 			break
 
 			case bchainlibs.InternalPing:
-				log.Info("Receiving PING from router with TID = " + tid)
-				payload := bchainlibs.AssemblePong(me)
+				log.Info("Receiving PING from router with ID = " + id)
+				payload := bchainlibs.BuildPong(me)
 				toOutput(payload)
 			break
 
@@ -199,7 +193,7 @@ func selectLeaderOfTheManet() {
 		log.Info("The leader has been chosen!!! All hail the new KING!!! " + me.String())
 		time.Sleep(time.Second * 5)
 
-		query := bchainlibs.AssembleQuery(me, "function")
+		query := bchainlibs.BuildQuery(me, "function")
 		toOutput(query)
 		log.Info("QUERY_START=" + strconv.FormatInt(time.Now().UnixNano(), 10))
 	}
@@ -216,7 +210,6 @@ func main() {
 
 	targetSync := c.TargetSync
 	logPath := c.LogPath
-	cryptoPiece = c.CryptoPiece
 	rootNode = c.RootNode
 
 	// Logger configuration
